80d7522d2887dae5994df506226a767c8dcbcf77
Nenad199822 <neso.zabjelo@gmail.com>
2023-08-21 17:36:33 +0200

    Make get computer choice function to make random choice between rock paper and scissors

--- javascript.js
+++ javascript.js
@@ -1,1 +1,6 @@
-console.log('Hello World');
+function getComputerChoice() {
+    randomChoice = ['Rock','Paper','Scissors'];
+    randomElement = Math.floor(Math.random() * randomChoice.length);
+    return randomChoice[randomElement];
+}
+console.log(getComputerChoice())
