bbb82ee62fd2b5bf977f0299b650236b444b4d15
Nenad199822 <neso.zabjelo@gmail.com>
2023-08-21 18:13:27 +0200

    Finish playRound function make how to choose winner with conditions and now move on next part

--- javascript.js
+++ javascript.js
@@ -8,7 +8,21 @@ function playerChoice() {
     let choiceToLower = userChoice.toLowerCase()
     return choiceToLower[0].toUpperCase() + choiceToLower.slice(1)
 }
-console.log(playerChoice())
-function singleRound(playerSelection, computerSelection) {
 
+function playRound(playerSelection, computerSelection) {
+
+    if (playerSelection === computerSelection) {
+        return "It's a tie!"
+    } else if (
+        (playerSelection === 'Rock' && computerSelection === 'Scissors') ||
+        (playerSelection === 'Paper' && computerSelection === 'Rock') ||
+        (playerSelection === 'Scissors' && computerSelection === 'Paper')
+    ) {
+        return (`You win ${playerSelection} beat ${computerSelection}`);
+    } else {
+        return (`You lose ${computerSelection} beat ${playerSelection}`)
+    }
 }
+const playerSelection = playerChoice();
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
